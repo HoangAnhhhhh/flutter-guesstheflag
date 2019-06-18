46a61ecc9220a2b9be9f4890809ea70f0084afc2
Hoang Anh <hoanganh@upupapp.asia>
2019-06-18 19:09:37 +0700

    created User model

--- lib/src/models/user.dart
+++ lib/src/models/user.dart
@@ -1,2 +1,25 @@
-class User{
+class User {
+  String id;
+  String name;
+  String email;
+  String photoURL;
+  int score;
+  DateTime dateCreated;
+  String ref;
+
+  User(this.id, this.name, this.email, this.photoURL, this.score,
+      this.dateCreated, this.ref);
+  
+  Map<String, dynamic> toJSON(){
+    Map<String, dynamic> user = {
+      'id': this.id,
+      'name': this.name,
+      'email': this.email,
+      'photoURL': this.photoURL,
+      'score': this.score,
+      'dateCreated': this.dateCreated,
+      'ref': this.ref,
+    };
+    return user;
+  }
 }
