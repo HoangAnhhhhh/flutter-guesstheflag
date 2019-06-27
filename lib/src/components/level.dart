d27e12616f7095d521555958f7cdc0ec589de942
Hoang Anh <hoanganh@upupapp.asia>
2019-06-27 23:01:55 +0700

    added animation to level page

--- lib/src/components/level.dart
+++ lib/src/components/level.dart
@@ -10,126 +10,138 @@ class Level extends StatefulWidget {
 }
 
 class _Level extends State<Level> with TickerProviderStateMixin {
-  AnimationController animationController;
-  Animation<double> animation;
+  AnimationController _controller;
+  Animation _basicAnimation, _transformAnimation;
 
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(
-        duration: Duration(milliseconds: 750), vsync: this);
-    animation =
-        CurvedAnimation(parent: animationController, curve: Curves.easeIn);
-    animationController.forward();
+    this._controller = AnimationController(
+        duration: Duration(milliseconds: 3000), vsync: this);
+    this._basicAnimation = Tween(begin: 10.0, end: double.infinity).animate(
+        CurvedAnimation(parent: this._controller, curve: Curves.ease));
+    this._transformAnimation = BorderRadiusTween(
+            begin: BorderRadius.circular(125.0),
+            end: BorderRadius.circular(1.0))
+        .animate(
+            CurvedAnimation(parent: this._controller, curve: Curves.ease));
+    this._controller.forward();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget>[
-        SizedBox(
-          height: 10,
-        ),
-        Stack(
-          children: <Widget>[
-            GestureDetector(
-              child: Container(
-                width: double.infinity,
-                child: FadeTransition(
-                  opacity: this.animation,
-                  child: Image.asset('assets/images/home/easy.jpeg',
-                      fit: BoxFit.fitWidth),
-                ),
+    return AnimatedBuilder(
+        animation: this._controller,
+        builder: (BuildContext context, Widget child) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 10,
               ),
-            ),
-            Positioned(
-                bottom: 100.0,
-                left: 10.0,
-                right: 10.0,
-                child: Center(
-                    child: RaisedButton(
-                  splashColor: Colors.amber,
-                  textColor: Colors.amberAccent,
-                  color: Colors.black87,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(10.0)),
-                  child: Text(
-                    "Easy",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 35.0,
-                      fontWeight: FontWeight.bold,
+              Stack(
+                children: <Widget>[
+                  GestureDetector(
+                    child: Container(
+                      width: this._basicAnimation.value,
+                      height: 280.0,
+                      decoration: BoxDecoration(
+                          borderRadius: this._transformAnimation.value,
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/home/easy.jpeg'),
+                              fit: BoxFit.fitWidth)),
                     ),
                   ),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => Play(level: 'Easy')));
-                  },
-                ))),
-          ],
-        ),
-        SizedBox(
-            height: 65,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Choose Level',
-                  textDirection: TextDirection.ltr,
-                  style: TextStyle(fontSize: 25.0, shadows: <Shadow>[
-                    Shadow(
-                      offset: Offset(2.0, 2.0),
-                      blurRadius: 3.0,
-                      color: Colors.amberAccent,
-                    ),
-                  ]),
-                ),
-              ],
-            )),
-        Stack(
-          children: <Widget>[
-            GestureDetector(
-              child: Container(
-                width: double.infinity,
-                child: FadeTransition(
-                  opacity: this.animation,
-                  child: Image.asset('assets/images/home/medium.jpeg',
-                      fit: BoxFit.fitWidth),
-                ),
+                  Positioned(
+                      bottom: 100.0,
+                      left: 10.0,
+                      right: 10.0,
+                      child: Center(
+                          child: RaisedButton(
+                        splashColor: Colors.amber,
+                        textColor: Colors.amberAccent,
+                        color: Colors.black87,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(10.0)),
+                        child: Text(
+                          "Easy",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 35.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Play(level: 'Easy')));
+                        },
+                      ))),
+                ],
               ),
-            ),
-            Positioned(
-                bottom: 100.0,
-                left: 10.0,
-                right: 10.0,
-                child: Center(
-                    child: RaisedButton(
-                  splashColor: Colors.amber,
-                  textColor: Colors.amberAccent,
-                  color: Colors.black87,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(10.0)),
-                  child: Text(
-                    "Medium",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 35.0,
-                      fontWeight: FontWeight.bold,
+              SizedBox(
+                  height: 65,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Choose Level',
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(fontSize: 25.0, shadows: <Shadow>[
+                          Shadow(
+                            offset: Offset(2.0, 2.0),
+                            blurRadius: 3.0,
+                            color: Colors.amberAccent,
+                          ),
+                        ]),
+                      ),
+                    ],
+                  )),
+              Stack(
+                children: <Widget>[
+                  GestureDetector(
+                    child: Container(
+                      width: this._basicAnimation.value,
+                      height: 280.0,
+                      decoration: BoxDecoration(
+                          borderRadius: this._transformAnimation.value,
+                          image: DecorationImage(
+                              image:
+                                  AssetImage('assets/images/home/medium.jpeg'),
+                              fit: BoxFit.fitWidth)),
                     ),
                   ),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => Play(level: 'Medium')));
-                  },
-                ))),
-          ],
-        ),
-      ],
-    );
+                  Positioned(
+                      bottom: 100.0,
+                      left: 10.0,
+                      right: 10.0,
+                      child: Center(
+                          child: RaisedButton(
+                        splashColor: Colors.amber,
+                        textColor: Colors.amberAccent,
+                        color: Colors.black87,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(10.0)),
+                        child: Text(
+                          "Medium",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 35.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Play(level: 'Medium')));
+                        },
+                      ))),
+                ],
+              ),
+            ],
+          );
+        });
   }
 }
