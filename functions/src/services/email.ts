9c5d9debf821c4403ab46b931b6409f15b4f3b44
Hoang Anh <hoanganh@upupapp.asia>
2019-07-01 19:49:15 +0700

    updated cloud function

--- functions/src/services/email.ts
+++ functions/src/services/email.ts
@@ -4,40 +4,28 @@ export class Email {
     private passwordAdmin: string = 'hoanganH123@';
     private emailTo: string;
     private displayNameTo: string;
-    private transporter: any;
-    mailOptions: any;
 
     constructor(email: string, displayName: string) {
         this.emailTo = email;
         this.displayNameTo = displayName;
-        this.initEmailSystem(this.emailAdmin, this.passwordAdmin);
     }
-
-    private initEmailSystem(emailAdmin: string, passwordAdmin: string): void {
-        this.transporter = nodemailer.createTransport({
+    public sendEmail(): void {
+        var transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-                user: emailAdmin,
-                pass: passwordAdmin
+                user: this.emailAdmin,
+                pass: this.passwordAdmin
             }
         });
-    }
 
-    private initEmailOptions(emailAdmin: string, emailTo: string, displayNameTo: string): any {
-        return this.mailOptions = {
-            from: emailAdmin,
-            to: emailTo,
-            subject: 'Send Welcome Email',
-            text:
-                `Xin chào ${displayNameTo},
-                Chào mừng tới với Guess The Flag game do Hoang Anh đã tạo ra. Rất mong bạn có thời gian vui vẻ khi trải nghiệm app của chúng tôi
-                `
+        var mailOptions = {
+            from: this.emailAdmin,
+            to: this.emailTo,
+            subject: 'Gửi email dùng Node.js --- dammio.com',
+            text: `Xin chào ${this.displayNameTo}, đây là email gửi bằng Node.js --- dammio.com`
         };
-    }
 
-    public sendEmail(): void {
-        const options: any = this.initEmailOptions(this.emailAdmin, this.emailTo, this.displayNameTo);
-        this.transporter.sendMail(options, function (error: any, info: any) {
+        transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
                 console.log(error);
             } else {
